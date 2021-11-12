f2e42c924d77a4011cb4c7d242e420851be71783
Julien ichard WCS <julien.y.richard@gmail.com>
2021-11-12 15:06:38 +0100

    Step 1: création et ajout del'object context et son provider pour les langues

---
+++ src/contexts/LanguagesContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from "react";
+
+export const LangagesContext = createContext();
+
+export const LangagesContextProvider = ({ children }) => {
+  const [langues, setLangues] = useState("en");
+
+  return (
+    <LangagesContext.Provider value={{ langues, setLangues }}>
+      {children}
+    </LangagesContext.Provider>
+  );
+};
