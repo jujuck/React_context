03d3e94a811be8da4d7f99bf98d857f855966407
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2022-12-01 14:55:22 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProverbsPage from './components/ProverbsPage';
 import SettingsPage from './components/SettingsPage';
 
 function App() {
-  const [proverbs, setProverbs] = useState([]);
+  //To be instanciate in the languageContext that you should do
   const [currentLanguage, setCurrentLanguage] = useState('en');
-
-  const fetchProverbs = () => {
-    console.log(`fetching "${currentLanguage}" proverbs`);
-    fetch(`/pretend-api/results-${currentLanguage}.json`)
-      .then((res) => res.json())
-      .then((data) => setProverbs(data.results));
-  };
-
-  // TODO : instanciate the two contexts providers here
-  // TODO : for exemple, you should pass proverbs, setProverbs and fetchProverbs to the ProverbsContext.Provider's value prop
 
   return (
     <Router>
       <Routes>
-        <Route path="/" exact component={ProverbsPage} />
-        <Route path="/settings" component={SettingsPage} />
+        <Route path="/" element={<ProverbsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </Router>
   );
