0794a7f2e409655fd5c23b87fb5fb132d87691dd
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2022-12-01 14:50:26 +0100

    Update ProverbsPage.jsx

--- src/components/ProverbsPage.jsx
+++ src/components/ProverbsPage.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getTranslatorForLanguage } from '../translations';
 
 const ProverbsPage = () => {
   // TODO: change this to something else, coming from the context
-  const proverbs = [];
-  // TODO: fetch the proverbs
+  const [proverbs, setProverbs] = useState([]);
+
+  useEffect(() => {
+    console.log(`fetching "${currentLanguage}" proverbs`);
+    fetch(`fake-api/${currentLanguage}.json`)
+      .then((res) => res.json())
+      .then((data) => setProverbs(data.results));
+  }, []);
 
   // TODO: change that to take the current language from Context instead of hardcoding "en"
   const t = getTranslatorForLanguage('en');
